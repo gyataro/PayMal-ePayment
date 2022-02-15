93dc0e0dfe1751f10c5f15e754bd5b9a96365162
gyataro <teoh6g@gmail.com>
2022-02-15 23:33:57 +0800

    Load config from .env file

---
+++ util/config.go
@@ -0,0 +1,27 @@
+package util
+
+import "github.com/spf13/viper"
+
+// Stores all configuration of the backend application
+type Config struct {
+	DBDriver      string `mapstructure:"DB_DRIVER"`
+	DBSource      string `mapstructure:"DB_SOURCE"`
+	ServerAddress string `mapstructure:"SERVER_ADDRESS"`
+}
+
+// LoadConfig reads configurations from file/environment variables
+func LoadConfig(path string) (config Config, err error) {
+	viper.AddConfigPath(path)
+	viper.SetConfigName("app")
+	viper.SetConfigType("env")
+
+	viper.AutomaticEnv()
+
+	err = viper.ReadInConfig()
+	if err != nil {
+		return
+	}
+
+	err = viper.Unmarshal(&config)
+	return
+}
