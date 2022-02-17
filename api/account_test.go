9141994c233ff1b3970b07a8681a88df5c1271b9
gyataro <teoh6g@gmail.com>
2022-02-17 21:54:18 +0800

    Added login API with test

--- api/account_test.go
+++ api/account_test.go
@@ -90,7 +90,7 @@ func TestGetAccountAPI(t *testing.T) {
 			store := mockdb.NewMockStore(ctrl)
 			tc.buildStubs(store)
 
-			server := NewServer(store)
+			server := NewTestServer(t, store)
 			recorder := httptest.NewRecorder()
 
 			url := fmt.Sprintf("/api/accounts/%d", tc.accountID)
@@ -177,7 +177,7 @@ func TestCreateAccountAPI(t *testing.T) {
 			store := mockdb.NewMockStore(ctrl)
 			tc.buildStubs(store)
 
-			server := NewServer(store)
+			server := NewTestServer(t, store)
 			recorder := httptest.NewRecorder()
 
 			// Marshal body data to JSON
@@ -291,7 +291,7 @@ func TestListAccountsAPI(t *testing.T) {
 			store := mockdb.NewMockStore(ctrl)
 			tc.buildStubs(store)
 
-			server := NewServer(store)
+			server := NewTestServer(t, store)
 			recorder := httptest.NewRecorder()
 
 			url := "/api/accounts"
--- api/server.go
+++ api/server.go
@@ -1,26 +1,47 @@
 package api
 
 import (
+	"fmt"
+
 	"github.com/gin-gonic/gin"
 	"github.com/gin-gonic/gin/binding"
 	"github.com/go-playground/validator/v10"
 	db "github.com/gyataro/paymal/db/sqlc"
+	"github.com/gyataro/paymal/token"
+	"github.com/gyataro/paymal/util"
 )
 
 // Server serves HTTP requests
 type Server struct {
-	store  db.Store
-	router *gin.Engine
+	config     util.Config
+	store      db.Store
+	tokenMaker token.Maker
+	router     *gin.Engine
 }
 
 // Creates new HTTP server instance
-func NewServer(store db.Store) *Server {
-	server := &Server{store: store}
-	router := gin.Default()
+func NewServer(config util.Config, store db.Store) (*Server, error) {
+	tokenMaker, err := token.NewPasetoMaker(config.TokenSymmetricKey)
+	if err != nil {
+		return nil, fmt.Errorf("cannot create token maker: %w", err)
+	}
+
+	server := &Server{
+		config:     config,
+		store:      store,
+		tokenMaker: tokenMaker,
+	}
 
 	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
 		v.RegisterValidation("currency", validCurrency)
 	}
+
+	server.setupRouter()
+	return server, nil
+}
+
+func (server *Server) setupRouter() {
+	router := gin.Default()
 
 	router.POST("/api/accounts", server.createAccount)
 	router.GET("/api/accounts/:id", server.getAccount)
@@ -29,9 +50,9 @@ func NewServer(store db.Store) *Server {
 	router.POST("/api/transfers", server.createTransfer)
 
 	router.POST("/api/users", server.createUser)
+	router.POST("/api/users/login", server.loginUser)
 
 	server.router = router
-	return server
 }
 
 // Starts HTTP server on a specific address
